55c20dfe3174d353a1fa8541101d6365c1b75756
chenxin <chenxin_verified@163.com>
2026-01-01 17:02:31 +0800

    fix: 修复 9008 下 U-Boot 无法启动的问题（9008 下 HTTP Server 暂不可用）

--- u-boot-2016/arch/arm/cpu/armv7/qca/common/smem.c
+++ u-boot-2016/arch/arm/cpu/armv7/qca/common/smem.c
@@ -642,12 +642,12 @@ int smem_get_boot_flash(uint32_t *flash_type,
 				    flash_type, sizeof(uint32_t));
 	if (ret != 0) {
 		printf("smem: read flash type failed\n");
-		//*flash_type = SMEM_BOOT_NO_FLASH;
-		//为了在USB 9008模式救砖时可以启动httpd或直接启动系统，设置一个读取flash type失败的默认值
-		//针对SPI NOR的机子，可以将*flash_type = SMEM_BOOT_SPI_FLASH
-		//使用的时候取消下面两行的屏蔽
-		printf("smem: set flash type to deafult type SPI FLASH\n");
-		*flash_type = SMEM_BOOT_SPI_FLASH;
+		*flash_type = SMEM_BOOT_NO_FLASH;
+		// 为了在 USB 9008 模式救砖时可以启动httpd或直接启动系统，设置一个读取 flash type 失败的默认值
+		// 针对 SPI-NOR 的机子，可以将 *flash_type = SMEM_BOOT_SPI_FLASH，使用的时候取消下面两行的屏蔽
+		// FIXME: 将 flash_type 设置为 SMEM_BOOT_SPI_FLASH 会导致 U-Boot 无法正常启动
+		// printf("smem: set flash type to deafult type SPI FLASH\n");
+		// *flash_type = SMEM_BOOT_SPI_FLASH;
 	}
 
 	ret = smem_read_alloc_entry(SMEM_BOOT_FLASH_INDEX,
